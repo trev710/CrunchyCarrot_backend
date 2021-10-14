c70059b4cf9d0fa341b82f2d8226c7c6efb27009
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-10-14 09:46:18 -0600

    model structuring

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+    belongs_to :movie, optional: true
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
+    
+    has_many :reviews
+    has_many :movies, through: :reviews
+
+    has_many :followed_users, foreign_key: :follower_id, class_name: 'Friendship'
+    has_many :followees, through: :followed_users
+
+    has_many :following_users, foreign_key: :followee_id, class_name: 'Friendship'
+    has_many :followers, through: :following_users
+  end
