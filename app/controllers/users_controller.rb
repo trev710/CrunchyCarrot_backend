2c026e653b1827c181e3744a42ba6b4012c0b5af
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-10-15 11:53:08 -0600

    auth and signup working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
     before_action :authenticate, only: [:show, :update]
 
+    def index
+        @users = User.all
+        render json: @users
+    end
+
     def show
         render json: @current_user
     end
@@ -11,18 +16,8 @@ class UsersController < ApplicationController
     end
 
     def update
-        @current_user.update(user_params)
-        render json: @current_user
-    end
-
-    def create
-        user = User.create(user_params)
-        render json: user
-    end
-
-    def signup
-        @user = User.create(username: params[:username], password: params[:password], avatar: params[:avatar])
-        # user = User.last
+        @user = User.find(params[:id]) 
+        @user.update(user_params)
         render json: @user
     end
 
@@ -37,9 +32,25 @@ class UsersController < ApplicationController
         render json: @user
     end
 
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+    def signup
+        @user = User.create(username: params[:username], password: params[:password], avatar: params[:avatar])
+        # user = User.last
+        render json: @user
+    end
+
+    def destroy
+        @user = User.find(params[:id]) 
+        @user.destroy
+        render json: @user
+    end
     private
 
     def user_params
-        params.permit(username, password, avatar)
+        params.permit(:username, :password, :avatar)
     end
 end
